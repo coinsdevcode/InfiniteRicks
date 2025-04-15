47b8d69af86b882923e2aababd9785bce848a431
coindev <webmastercase20@gmail.com>
2025-04-14 22:54:04 -0300

    update

--- src/util.cpp
+++ src/util.cpp
@@ -1046,6 +1046,44 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
     return path;
 }
 
+string randomStrGen(int length) {
+     static string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+     string result;
+     result.resize(length);
+     for (int32_t i = 0; i < length; i++)
+         result[i] = charset[rand() % charset.length()];
+ 
+     return result;
+ }
+ 
+ void createConf()
+ {
+     srand(static_cast<unsigned int>(time(NULL)));
+ 
+     ofstream pConf;
+ #if BOOST_FILESYSTEM_VERSION >= 3
+     pConf.open(GetConfigFile().generic_string().c_str());
+ #else
+     pConf.open(GetConfigFile().string().c_str());
+ #endif
+     pConf << "rpcuser=user"
+             + randomStrGen(15)
+             + "\nrpcpassword="
+             + randomStrGen(15)
+             + "\n#(0=off, 1=on) staking - turn staking on or off"
+             + "\nstaking=1"
+             + "\nlisten=1"
+             + "\ndaemon=1"
+             + "\nserver=1"
+             + "\nrpcallowip=127.0.0.1"
+             + "\naddnode=75.119.137.26"
+             + "\naddnode=62.163.210.67"
+             + "\naddnode=82.64.109.25"
+             + "\naddnode=136.38.237.162"
+             + "\naddnode=202.169.105.168";
+     pConf.close();
+ }
+ 
 boost::filesystem::path GetConfigFile()
 {
     boost::filesystem::path pathConfigFile(GetArg("-conf", "InfiniteRicks.conf"));
@@ -1058,8 +1096,13 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 {
     boost::filesystem::ifstream streamConfig(GetConfigFile());
     if (!streamConfig.good())
-        return; // No bitcoin.conf file is OK
-
+        {
+         createConf();
+         new(&streamConfig) boost::filesystem::ifstream(GetConfigFile());
+         if(!streamConfig.good())
+             return;
+ 	}
+ 	
     set<string> setOptions;
     setOptions.insert("*");
 
